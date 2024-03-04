cb695dc3a6942c9db5777b56acf96189f67d5431
Jez <5053926+ninjars@users.noreply.github.com>
2024-03-04 17:05:00 +0000

    added icon and image loading dependency and permission

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,6 +58,8 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3:1.2.0")
+    implementation("io.coil-kt:coil-compose:2.6.0")
+    implementation("androidx.compose.material:material-icons-core:1.6.2")
 
     testImplementation("junit:junit:4.13.2")
 
