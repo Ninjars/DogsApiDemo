80a6f657c986854197943c2690a085e8251b08cf
Jez <5053926+ninjars@users.noreply.github.com>
2024-03-05 12:46:05 +0000

    refactored loading screen to separate component to enable reuse

--- app/src/main/java/net/jeremystevens/dogs/ui/screens/BreedsListScreen.kt
+++ app/src/main/java/net/jeremystevens/dogs/ui/screens/BreedsListScreen.kt
@@ -1,8 +1,6 @@
 package net.jeremystevens.dogs.ui.screens
 
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -10,21 +8,12 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.pulltorefresh.PullToRefreshContainer
-import androidx.compose.material3.pulltorefresh.rememberPullToRefreshState
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.input.nestedscroll.nestedScroll
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import net.jeremystevens.dogs.R
+import net.jeremystevens.dogs.ui.components.Refreshable
 import net.jeremystevens.dogs.ui.screens.BreedsListState.BreedsListContent
 import net.jeremystevens.dogs.ui.theme.DogsTheme
 
@@ -58,28 +47,10 @@ fun BreedsListStateSwitcher(
 ) {
     when (state) {
         is BreedsListContent -> BreedsListContent(state, eventHandler)
-        is BreedsListState.Loading -> LoadingState()
+        is BreedsListState.Loading -> LoadingScreen()
     }
 }
 
-@Composable
-private fun LoadingState() {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
-        modifier = Modifier.fillMaxSize()
-    ) {
-        Text(
-            text = stringResource(R.string.breeds_loading_message),
-            style = MaterialTheme.typography.headlineSmall,
-            textAlign = TextAlign.Center,
-            modifier = Modifier.padding(16.dp)
-        )
-        CircularProgressIndicator()
-    }
-}
-
-@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun BreedsListContent(
     state: BreedsListContent,
@@ -182,17 +153,6 @@ private fun DogBreedScreenRefreshingPreview() {
                 ),
                 isRefreshing = true,
             ),
-            eventHandler = {},
-        )
-    }
-}
-
-@Preview(apiLevel = 33, showBackground = true)
-@Composable
-private fun DogBreedScreenLoadingPreview() {
-    DogsTheme {
-        BreedsListStateSwitcher(
-            state = BreedsListState.Loading,
             eventHandler = {},
         )
     }
---
+++ app/src/main/java/net/jeremystevens/dogs/ui/screens/LoadingScreen.kt
@@ -0,0 +1,43 @@
+package net.jeremystevens.dogs.ui.screens
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import net.jeremystevens.dogs.R
+import net.jeremystevens.dogs.ui.theme.DogsTheme
+
+@Composable
+fun LoadingScreen() {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Text(
+            text = stringResource(R.string.breeds_loading_message),
+            style = MaterialTheme.typography.headlineSmall,
+            textAlign = TextAlign.Center,
+            modifier = Modifier.padding(16.dp)
+        )
+        CircularProgressIndicator()
+    }
+}
+
+@Preview(apiLevel = 33, showBackground = true)
+@Composable
+private fun BreedPhotosScreenPreview() {
+    DogsTheme {
+        LoadingScreen()
+    }
+}
